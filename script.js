be6450193b2430f0d256e7d2d52c1e693472dee7
zahi79027 <zahizahir70@gmail.com>
2025-08-20 23:41:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function showPoster(src){
-  const modal = document.getElementById('posterModal');
-  const img = document.getElementById('posterImg');
-  modal.classList.add('show');
-  img.src = src;
-}
-function closePoster(){
-  document.getElementById('posterModal').classList.remove('show');
-}
+
